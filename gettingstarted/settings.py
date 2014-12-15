cd9233afeac70e53efa0556f5cb3d9178a813e87
psultan <psultan@gmail.com>
2014-12-14 21:34:33 -0500

    testing staticfiles

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -96,9 +96,9 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+print BASE_DIR
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../hello/static'),
+    os.path.join(BASE_DIR, 'hello/static'),
 )
