2340494ccd3548f152a30b90ad92c97f3d13124d
psultan <psultan@gmail.com>
2014-12-14 21:24:35 -0500

    testing staticfiles

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -100,6 +100,5 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-	os.path.join('hello/static/images'),
+    os.path.join(BASE_DIR, '../hello/static'),
 )
