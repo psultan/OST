3122ff0346e82fafd690cbb1901f73336005ef5a
psultan <psultan@gmail.com>
2014-12-17 14:51:53 -0500

    trying to fix readthe docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,12 +15,16 @@
 import sys
 import os
 
+
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../../hello'))
 sys.path.insert(0, os.path.abspath('../../gettingstarted'))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+
 
 # -- General configuration ------------------------------------------------
 
